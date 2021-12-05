a4b7a38a9ddac67eb2a8547e35eda08d619b6975
tattobu28 <tattobu3989@gmail.com>
2021-12-05 16:51:38 +0900

    artistテーブルの作成

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,4 @@
 class Artist < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
