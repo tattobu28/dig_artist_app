df4de5360a653af8cf6c1969a74d66ef5a1ef939
tattobu28 <tattobu3989@gmail.com>
2021-12-05 17:07:43 +0900

    バリデーションの設定

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,5 +3,9 @@ class Artist < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :artist_id, presence: true
-  
+  validates :name,      presence: true
+  validates :member,    presence: true
+  validates :city,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :genre,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+
 end
