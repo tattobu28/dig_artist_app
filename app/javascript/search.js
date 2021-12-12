53f3f69b31a91b7ac1c3b208192ffd1ab27634c4
tattobu28 <tattobu3989@gmail.com>
2021-12-13 00:24:21 +0900

    検索機能フロント実装

---
+++ app/javascript/search.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("search")
+  const pullDownParents = document.getElementById("pull-down")
+
+  pullDownButton.addEventListener('click', function() {
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+})
