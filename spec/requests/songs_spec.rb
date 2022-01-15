0ae395da064548a58730c37ee6513b754bb5fbdc
tattobu28 <tattobu3989@gmail.com>
2022-01-15 21:50:05 +0900

    Update songs_spec.rb

--- spec/requests/songs_spec.rb
+++ spec/requests/songs_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 describe SongsController, type: :request do
-
   before do
     @song = FactoryBot.create(:song)
     @artist = FactoryBot.create(:artist)
@@ -8,56 +7,56 @@ describe SongsController, type: :request do
   end
 
   describe 'GET #index' do
-      it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
-        get root_path
-        expect(response.status).to eq 200
-      end
-      it 'indexアクションにリクエストするとレスポンスにアップロード済みの楽曲のタイトルが存在する' do 
-        get root_path
-        expect(response.body).to include(@song.title)
-      end
-      it 'indexアクションにリクエストするとレスポンスにアップロード済みの楽曲の画像が存在する' do 
-        # get root_path
-        # expect(action.body).to include(url_for(@song.image.variant(resize: '500x500')))
-      end
-      it 'indexアクションにリクエストするとレスポンスにアップロード済みの楽曲のアーティスト名が存在する' do 
-        get root_path
-        expect(response.body).to include(@song.artist.name)
-      end
-      it 'indexアクションにリクエストするとレスポンスに登録済みのアーティスト名が存在する' do 
-        get root_path
-        expect(response.body).to include(@artist.name)
-      end
-      it 'indexアクションにリクエストするとレスポンスに登録済みのアーティストのジャンルが存在する' do 
-        get root_path
-        expect(response.body).to include(@artist.genre.name)
-      end
-      it 'indexアクションにリクエストするとレスポンスに登録済みのアーティストのジャンルが存在する' do 
-        get root_path
-        expect(response.body).to include(@artist.city.name)
-      end
-      it 'indexアクションにリクエストするとレスポンスに登録済みのアーティストの画像が存在する' do 
-        # get root_path
-        # expect(response.body).to include(@artist.image)
-      end
-      it 'indexアクションにリクエストするとレスポンスにアップロード済みのMVのurlが存在する' do 
-        get root_path
-        expect(response.body).to include(@youtube.url)
-      end
-      it 'indexアクションにリクエストするとレスポンスにアップロード済みのMVのタイトルが存在する' do 
-        get root_path
-        expect(response.body).to include(@youtube.title)
-      end
-      it 'indexアクションにリクエストするとレスポンスにアップロード済みのMVのアーティスト名が存在する' do 
-        get root_path
-        expect(response.body).to include(@youtube.artist.name)
-      end
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get root_path
+      expect(response.status).to eq 200
+    end
+    it 'indexアクションにリクエストするとレスポンスにアップロード済みの楽曲のタイトルが存在する' do
+      get root_path
+      expect(response.body).to include(@song.title)
+    end
+    it 'indexアクションにリクエストするとレスポンスにアップロード済みの楽曲の画像が存在する' do
+      # get root_path
+      # expect(action.body).to include(url_for(@song.image.variant(resize: '500x500')))
+    end
+    it 'indexアクションにリクエストするとレスポンスにアップロード済みの楽曲のアーティスト名が存在する' do
+      get root_path
+      expect(response.body).to include(@song.artist.name)
+    end
+    it 'indexアクションにリクエストするとレスポンスに登録済みのアーティスト名が存在する' do
+      get root_path
+      expect(response.body).to include(@artist.name)
+    end
+    it 'indexアクションにリクエストするとレスポンスに登録済みのアーティストのジャンルが存在する' do
+      get root_path
+      expect(response.body).to include(@artist.genre.name)
+    end
+    it 'indexアクションにリクエストするとレスポンスに登録済みのアーティストのジャンルが存在する' do
+      get root_path
+      expect(response.body).to include(@artist.city.name)
+    end
+    it 'indexアクションにリクエストするとレスポンスに登録済みのアーティストの画像が存在する' do
+      # get root_path
+      # expect(response.body).to include(@artist.image)
+    end
+    it 'indexアクションにリクエストするとレスポンスにアップロード済みのMVのurlが存在する' do
+      get root_path
+      expect(response.body).to include(@youtube.url)
+    end
+    it 'indexアクションにリクエストするとレスポンスにアップロード済みのMVのタイトルが存在する' do
+      get root_path
+      expect(response.body).to include(@youtube.title)
+    end
+    it 'indexアクションにリクエストするとレスポンスにアップロード済みのMVのアーティスト名が存在する' do
+      get root_path
+      expect(response.body).to include(@youtube.artist.name)
+    end
     context 'ログインしていない場合' do
-      it 'indexアクションにリクエストするとレスポンスにログインボタンが存在する' do 
+      it 'indexアクションにリクエストするとレスポンスにログインボタンが存在する' do
         get root_path
         expect(response.body).to include('ログイン')
       end
-      it 'indexアクションにリクエストするとレスポンスに新規登録ボタンが存在する' do 
+      it 'indexアクションにリクエストするとレスポンスに新規登録ボタンが存在する' do
         get root_path
         expect(response.body).to include('新規登録')
       end
@@ -68,15 +67,15 @@ describe SongsController, type: :request do
       end
       it 'indexアクションにリクエストするとレスポンスにログアウトボタンが存在する' do
         get root_path
-        expect(response.body).to include('ログアウト')  
+        expect(response.body).to include('ログアウト')
       end
       it 'indexアクションにリクエストするとレスポンスに楽曲アップロードボタンが存在する' do
         get root_path
-        expect(response.body).to include('楽曲アップロード')  
+        expect(response.body).to include('楽曲アップロード')
       end
       it 'indexアクションにリクエストするとレスポンスにMVアップロードボタンが存在する' do
         get root_path
-        expect(response.body).to include('MVアップロード')  
+        expect(response.body).to include('MVアップロード')
       end
     end
   end
